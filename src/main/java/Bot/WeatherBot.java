e7fd3663fcd13b0c7cad365beba65e9e61ddeb2c
yanomaly <family.tolstoy@gmail.com>
2022-04-04 18:03:58 +0300

    Forecasts done

--- src/main/java/Bot/WeatherBot.java
+++ src/main/java/Bot/WeatherBot.java
@@ -16,6 +16,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -59,6 +60,8 @@ public class WeatherBot extends TelegramLongPollingBot  {
                     help(update);
                 if(update.getCallbackQuery().getData().equals("Menu"))
                     menu(update);
+                if(update.getCallbackQuery().getData().equals("/menu"))
+                    menuInline(update);
                 if(update.getCallbackQuery().getData().equals("FindCity"))
                     findCity(update);
                 if(update.getCallbackQuery().getData().equals("Subscribe"))
@@ -179,11 +182,16 @@ public class WeatherBot extends TelegramLongPollingBot  {
         add.add(Arrays.asList(button5));
         markupInline.setKeyboard(add);
         SendMessage outMessage = new SendMessage();
-        outMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+        String id;
+        if(update.getMessage() != null)
+        id = String.valueOf(update.getMessage().getChatId());
+        else
+        id = String.valueOf(update.getCallbackQuery().getMessage().getChatId());
+        outMessage.setChatId(id);
         outMessage.setReplyMarkup(markupInline);
         outMessage.setText("\uD83C\uDF2C    Main menu   \uD83C\uDF2C");
         PreparedStatement statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 0 WHERE chatid = ?");
-        statement.setInt(1, update.getMessage().getChatId().intValue());
+        statement.setInt(1, Integer.parseInt(id));
         statement.execute();
         execute(outMessage);
     }
@@ -231,15 +239,36 @@ public class WeatherBot extends TelegramLongPollingBot  {
             object = new JSONObject(result);
         }
         if(fl){
-            EditMessageText editText = new EditMessageText();
+            SendMessage editText = new SendMessage();
             InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-            editText.setMessageId(update.getMessage().getMessageId());
             editText.setChatId(String.valueOf(update.getMessage().getChatId()));
-            double temp = object.getJSONObject("main").getDouble("temp") - 273.15;
-            double real = object.getJSONObject("main").getDouble("feels_like") - 273.15;
-            editText.setText("\uD83C\uDFD9 City: " + city + "\n" +
+            String temp = new DecimalFormat("#0.00").format(object.getJSONObject("main").getDouble("temp") - 273.15);
+            String real = new DecimalFormat("#0.00").format(object.getJSONObject("main").getDouble("feels_like") - 273.15);
+            int deg = object.getJSONObject("wind").getInt("deg");
+            String dir = "";
+            if((deg >= 337 && deg <= 360) || (deg >= 0 && deg <= 22))
+                dir = "   \uD83D\uDD04 Direction: S ⬆";
+            if(deg > 22 && deg < 67)
+                dir = "   \uD83D\uDD04 Direction: SW ↗";
+            if(deg >= 67 && deg <= 112)
+                dir = "   \uD83D\uDD04 Direction: W ➡";
+            if(deg > 112 && deg < 157)
+                dir = "   \uD83D\uDD04 Direction: NW ↘";
+            if(deg >= 157 && deg <= 202)
+                dir = "   \uD83D\uDD04 Direction: N ⬇";
+            if(deg > 202 && deg < 247)
+                dir = "   \uD83D\uDD04 Direction: NE ↙";
+            if(deg >= 247 && deg <= 292)
+                dir = "   \uD83D\uDD04 Direction: E ⬅";
+            if(deg > 292 && deg < 337)
+                dir = "   \uD83D\uDD04 Direction: SE ↖";
+            editText.setText("\uD83C\uDFD9 City: " + city + ", " + object.getJSONObject("sys").getString("country") + "\n" +
                     "\uD83C\uDF21 Temperature: " + temp + " °С\n" +
                     "\uD83D\uDCAF Real feel: " + real + " °С\n" +
+                    "\uD83C\uDF8F Wind: \n" +
+                      "   \uD83C\uDFCE Speed: " + object.getJSONObject("wind").getDouble("speed") + " m/s\n" +
+                      "   \uD83D\uDCA8 Gust: " + object.getJSONObject("wind").getDouble("gust") + " m/s\n" +
+                       dir + "\n" +
                     "\uD83D\uDD28 Pressure: " + object.getJSONObject("main").getInt("pressure" ) + " mm. hg.\n" +
                     "\uD83D\uDCA7 Humidity: " + object.getJSONObject("main").getInt("humidity")+ " %\n" +
                     "\uD83E\uDD13 Description: " + object.getJSONArray("weather").getJSONObject(0).getString("main") + ", " + object.getJSONArray("weather").getJSONObject(0).getString("description")
@@ -247,30 +276,29 @@ public class WeatherBot extends TelegramLongPollingBot  {
             List<List<InlineKeyboardButton>> add = new ArrayList<>();
             InlineKeyboardButton button2 = new InlineKeyboardButton();
             button2.setText("Menu \uD83D\uDCDD");
-            button2.setCallbackData("Menu");
+            button2.setCallbackData("/menu");
             add.add(Arrays.asList(button2));
             markupInline.setKeyboard(add);
             editText.setReplyMarkup(markupInline);
+            PreparedStatement statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 0 WHERE chatid = ?");
+            statement.setInt(1, update.getMessage().getChatId().intValue());
+            statement.execute();
             execute(editText);
         }
         else{
-            EditMessageText editText = new EditMessageText();
+            SendMessage editText = new SendMessage();
             InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-            editText.setMessageId(update.getMessage().getMessageId());
             editText.setChatId(String.valueOf(update.getMessage().getChatId()));
-            editText.setText("Sorry didn't find nothing about " + city +"\uD83D\uDE1E");
+            editText.setText("Sorry didn't find nothing about " + city + "\uD83D\uDE1E \nBut you can try again:");
             List<List<InlineKeyboardButton>> add = new ArrayList<>();
             InlineKeyboardButton button2 = new InlineKeyboardButton();
             button2.setText("Menu \uD83D\uDCDD");
-            button2.setCallbackData("Menu");
+            button2.setCallbackData("/menu");
             add.add(Arrays.asList(button2));
             markupInline.setKeyboard(add);
             editText.setReplyMarkup(markupInline);
             execute(editText);
         }
-        PreparedStatement statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 0 WHERE chatid = ?");
-        statement.setInt(1, update.getCallbackQuery().getMessage().getChatId().intValue());
-        statement.execute();
     }
     public void subscribe(Update update) throws TelegramApiException{
 
