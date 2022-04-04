7b960699ccf57803ec4ae12118e55e88079abfd4
yanomaly <family.tolstoy@gmail.com>
2022-04-04 22:49:58 +0300

    Subsribing done

--- src/main/java/Bot/WeatherBot.java
+++ src/main/java/Bot/WeatherBot.java
@@ -21,6 +21,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WeatherBot extends TelegramLongPollingBot  {
 
@@ -267,7 +269,6 @@ public class WeatherBot extends TelegramLongPollingBot  {
                     "\uD83D\uDCAF Real feel: " + real + " °С\n" +
                     "\uD83C\uDF8F Wind: \n" +
                       "   \uD83C\uDFCE Speed: " + object.getJSONObject("wind").getDouble("speed") + " m/s\n" +
-                      "   \uD83D\uDCA8 Gust: " + object.getJSONObject("wind").getDouble("gust") + " m/s\n" +
                        dir + "\n" +
                     "\uD83D\uDD28 Pressure: " + object.getJSONObject("main").getInt("pressure" ) + " mm. hg.\n" +
                     "\uD83D\uDCA7 Humidity: " + object.getJSONObject("main").getInt("humidity")+ " %\n" +
@@ -300,13 +301,141 @@ public class WeatherBot extends TelegramLongPollingBot  {
             execute(editText);
         }
     }
-    public void subscribe(Update update) throws TelegramApiException{
-
-    }
-    public void subscribe(Update update, int flag) throws TelegramApiException, SQLException {
-        PreparedStatement statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 0 WHERE chatid = ?");
+    public void subscribe(Update update) throws TelegramApiException, SQLException {
+        EditMessageText editText = new EditMessageText();
+        editText.setMessageId(update.getCallbackQuery().getMessage().getMessageId());
+        editText.setChatId(String.valueOf(update.getCallbackQuery().getMessage().getChatId()));
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> add = new ArrayList<>();
+        List<InlineKeyboardButton> addLine = new ArrayList<>();
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("Menu \uD83D\uDCDD");
+        button2.setCallbackData("Menu");
+        addLine.add(button2);
+        add.add(addLine);
+        markupInline.setKeyboard(add);
+        editText.setReplyMarkup(markupInline);
+        editText.setText("Please, input information about subscription this way: \nCity - XX:XX ");
+        PreparedStatement statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 2 WHERE chatid = ?");
         statement.setInt(1, update.getCallbackQuery().getMessage().getChatId().intValue());
         statement.execute();
+        execute(editText);
+    }
+    public void subscribe(Update update, int flag) throws TelegramApiException, SQLException, IOException {
+        String[] sub = update.getMessage().getText().split(" - ");
+        Pattern time = Pattern.compile("(0[0-9]:[0-5][0-9])|(1[0-9]:[0-5][0-9])|(2[0-3]:[0-5][0-9])");
+        if(sub.length == 2) {
+            if(time.matcher(sub[1]).matches()){
+                URL url = new URL("http://api.openweathermap.org/geo/1.0/direct?q=" + sub[0] + "&appid=c712fdd570b5fe58adcaec207334729e");
+                Scanner in = new Scanner((InputStream) url.getContent());
+                String result="";
+                while (in.hasNext()) {
+                    result += in.nextLine();
+                }
+                if(!result.equals("[]")) {
+                    String[] tm = sub[1].split(":");
+                    int minuts = Integer.parseInt(tm[0])*60+Integer.parseInt(tm[1]);
+                    String city = sub[0];
+                    PreparedStatement statement = dao.getConnection().prepareStatement("SELECT idsubs FROM subs WHERE time = ? AND city = ?");
+                    statement.setInt(1, minuts);
+                    statement.setString(2, city);
+                    if(!statement.executeQuery().next()){
+                        statement = dao.getConnection().prepareStatement("INSERT INTO subs(city, time) VALUES(?, ?)");
+                        statement.setInt(2, minuts);
+                        statement.setString(1, city);
+                        statement.execute();
+                    }
+                    statement = dao.getConnection().prepareStatement(
+                            "SELECT idusers_subs FROM user_subs WHERE " +
+                            "userid = (SELECT userid FROM users WHERE chatid = ?) AND " +
+                            "subid = (SELECT subid FROM subs WHERE time = ? AND city = ?)");
+                    statement.setInt(1, update.getMessage().getChatId().intValue());
+                    statement.setInt(2, minuts);
+                    statement.setString(3, city);
+                    if(!statement.executeQuery().next()){
+                        statement = dao.getConnection().prepareStatement("INSERT INTO user_subs (userid, subid) VALUES ((SELECT idusers FROM users WHERE chatid = ?),(SELECT idsubs FROM subs WHERE time = ? AND city = ?))");
+                        statement.setInt(1, update.getMessage().getChatId().intValue());
+                        statement.setInt(2, minuts);
+                        statement.setString(3, city);
+                        statement.execute();
+                        SendMessage editText = new SendMessage();
+                        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                        editText.setChatId(String.valueOf(update.getMessage().getChatId()));
+                        List<List<InlineKeyboardButton>> add = new ArrayList<>();
+                        InlineKeyboardButton button2 = new InlineKeyboardButton();
+                        button2.setText("Menu \uD83D\uDCDD");
+                        button2.setCallbackData("/menu");
+                        add.add(Arrays.asList(button2));
+                        markupInline.setKeyboard(add);
+                        editText.setReplyMarkup(markupInline);
+                        editText.setText("You successfully subscribed!\uD83D\uDE0A");
+                        statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 0 WHERE chatid = ?");
+                        statement.setInt(1, update.getMessage().getChatId().intValue());
+                        statement.execute();
+                        execute(editText);
+                    }
+                    else{
+                        SendMessage editText = new SendMessage();
+                        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                        editText.setChatId(String.valueOf(update.getMessage().getChatId()));
+                        List<List<InlineKeyboardButton>> add = new ArrayList<>();
+                        InlineKeyboardButton button2 = new InlineKeyboardButton();
+                        button2.setText("Menu \uD83D\uDCDD");
+                        button2.setCallbackData("/menu");
+                        add.add(Arrays.asList(button2));
+                        markupInline.setKeyboard(add);
+                        editText.setReplyMarkup(markupInline);
+                        editText.setText("You already subscribed on this forecast\uD83E\uDD13");
+                        statement = dao.getConnection().prepareStatement("UPDATE users SET flag = 0 WHERE chatid = ?");
+                        statement.setInt(1, update.getMessage().getChatId().intValue());
+                        statement.execute();
+                        execute(editText);
+                    }
+                }
+                else{
+                    SendMessage editText = new SendMessage();
+                    InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                    editText.setChatId(String.valueOf(update.getMessage().getChatId()));
+                    List<List<InlineKeyboardButton>> add = new ArrayList<>();
+                    InlineKeyboardButton button2 = new InlineKeyboardButton();
+                    button2.setText("Menu \uD83D\uDCDD");
+                    button2.setCallbackData("/menu");
+                    add.add(Arrays.asList(button2));
+                    markupInline.setKeyboard(add);
+                    editText.setReplyMarkup(markupInline);
+                    editText.setText("City invalid\uD83C\uDF06  \n Try again:");
+                    execute(editText);
+                }
+            }
+            else{
+                SendMessage editText = new SendMessage();
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                editText.setChatId(String.valueOf(update.getMessage().getChatId()));
+                List<List<InlineKeyboardButton>> add = new ArrayList<>();
+                InlineKeyboardButton button2 = new InlineKeyboardButton();
+                button2.setText("Menu \uD83D\uDCDD");
+                button2.setCallbackData("/menu");
+                add.add(Arrays.asList(button2));
+                markupInline.setKeyboard(add);
+                editText.setReplyMarkup(markupInline);
+                editText.setText("Time invalid\uD83D\uDD70 \n Try again:");
+                execute(editText);
+            }
+        }
+        else{
+            SendMessage editText = new SendMessage();
+            InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+            editText.setChatId(String.valueOf(update.getMessage().getChatId()));
+            List<List<InlineKeyboardButton>> add = new ArrayList<>();
+            InlineKeyboardButton button2 = new InlineKeyboardButton();
+            button2.setText("Menu \uD83D\uDCDD");
+            button2.setCallbackData("/menu");
+            add.add(Arrays.asList(button2));
+            markupInline.setKeyboard(add);
+            editText.setReplyMarkup(markupInline);
+            editText.setText("Data format invalid✍  \n Try again:");
+            execute(editText);
+        }
     }
     public void yourSubs(Update update) throws TelegramApiException{
 
